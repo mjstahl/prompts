020dd39894b485dc5f217051cd04fc441e3b83c4
Mark Stahl <mjstahl@gmail.com>
2019-10-20 18:38:15 -0400

    Fixed issue with progressId and the updated progress template

--- server/routes/progress.js
+++ server/routes/progress.js
@@ -9,18 +9,18 @@ module.exports = [{
   method: 'post',
   path: '/progress/:id/next',
   callback: function (req, res) {
-    const progressID = req.params.id
-    const progress = Progress[progressID]
+    const progressId = req.params.id
+    const progress = Progress[progressId]
     const onStep = progress.completed
 
     console.log(Object.entries(req.body))
     const [variable, value] = Object.entries(req.body)[0]
-    updateProgress(progressID, variable, value)
+    updateProgress(progressId, variable, value)
 
     const { input, prompt } = Lists[progress.list].steps[onStep + 1]
     const promptTemplate = handlebars.compile(prompt)
     const step = {
-      id: progressID,
+      progressId,
       input,
       prompt: promptTemplate(progress.input)
     }
