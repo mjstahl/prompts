ac21b0722bfd41473cc71e708c5025abd0c96713
Mark Stahl <mjstahl@gmail.com>
2019-10-23 20:34:26 -0400

    Trying to deploy to Heroku

--- server/index.js
+++ server/index.js
@@ -2,11 +2,13 @@ const express = require('express')
 const initRoutes = require('./routes/')
 const app = express()
 
-const PORT = 3000
-
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('client'))
 
 initRoutes(app)
-app.listen(PORT, () => console.log(`Prompts started on ${PORT}.`))
+
+const PORT = process.env.PORT || 8080
+app.listen(PORT, () => {
+  console.log(`Prompts started on ${PORT}.`)
+})
